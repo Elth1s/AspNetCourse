a726eb2e31fadddf3d5488c6542796a37c470f8f
Dima <dg646726@gmail.com>
2021-08-07 13:46:34 +0300

    Add LocalCommunitiesController

---
+++ AdminLTE.MVC/Models/ViewModels/EmployeeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.Models.ViewModels
+{
+    public class EmployeeVM
+    {
+        public Employee Employee { get; set; }
+        public IEnumerable<LocalCommunity> LocalCommunitySelectList { get; set; }
+    }
+}
